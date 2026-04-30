ebacd7f06bea0fe9fe85131cae1e537bdcc53421
sudjairakc <sudjairak.c@gmail.com>
2026-04-30 11:54:22 +0700

    fix: update CORS headers to include Authorization for improved API security

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func corsMiddleware(allowedOrigins string, next http.Handler) http.Handler {
 		if allowed != "" {
 			w.Header().Set("Access-Control-Allow-Origin", allowed)
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
 		}
 
